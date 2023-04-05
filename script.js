06210d48d373e11d01eef67061f521c71e16616b
Leo_Macedo <leonardo.aurieni@etec.sp.gov.br>
2023-04-05 15:43:49 -0300

    js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function alterar(){
-    var numale = Math.trunc(math.random()*10);
+    var numale = Math.trunc(Math.random()*10);
     var imagem = "img/Fortnite-" + numale + ".jpg";
     document.getElementById ("img").src = imagem;
 }
